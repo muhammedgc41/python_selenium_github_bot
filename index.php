89e56d7d691395b636f1eb827f2da652fd2f641b
muhammedgc41 <132597029+muhammedgc41@users.noreply.github.com>
2023-05-04 21:31:28 +0300

    falan filna
    burasıda açıklama bölümü

---
+++ index.php
@@ -0,0 +1,7 @@
+echo "# python_selenium_github_bot" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/muhammedgc41/python_selenium_github_bot.git
+git push -u origin main
