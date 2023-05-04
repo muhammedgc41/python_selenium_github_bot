916af4a046f5a302098bcc7974230ac6c5b289b9
muhammedgc41 <132597029+muhammedgc41@users.noreply.github.com>
2023-05-04 21:35:52 +0300

    python dosyası yüklendi

---
+++ index.py
@@ -0,0 +1,10 @@
+v class="row">
+        <div class="col">
+            <div class="conteiner-fluid">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Similique officia possimus voluptatibus quaerat illo omnis 
+                ad dicta ratione et dolore magni ducimus quas reiciendis 
+                magnam voluptate id repellendus, facere itaque?
+            </div>
+        </div>
+    </div>
