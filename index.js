125f385c94178234bfbf0490e76cad1198fbfcc4
muhammedgc41 <132597029+muhammedgc41@users.noreply.github.com>
2023-05-21 14:09:39 +0300

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-<!-- START OF SCRIPT -->
-<!-- Daha fazla kod için sitemizi ziyaret ediniz http://www.abecem.net  -->
-<a href="" onMouseover="document.bgColor=&apos;red&apos;">Kırmızı</a><br><a href="" onMouseover="document.bgColor=&apos;orange&apos;">Portakal</a><br> <a href="" onMouseover="document.bgColor=&apos;yellow&apos;">Sarı</a><br> <a href="" onMouseover="document.bgColor=&apos;green&apos;">Yeşil</a><br> <a href="" onMouseover="document.bgColor=&apos;blue&apos;">Mavi</a><br> <a href="" onMouseover="document.bgColor=&apos;purple&apos;">Mor</a><br> 
-<!-- END OF SCRIPT -->
